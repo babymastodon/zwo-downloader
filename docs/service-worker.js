b91de8c4916b7d64b3241edd35a8bd7926de34bf
Zachary Drach <zackdrach@gmail.com>
2025-12-01 21:56:31 -0800

    📚 docs — service-worker.js (+1/-1, 2025-12-01 21:56)

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // Bump this when you deploy a new version so clients pick up new files
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const CACHE_NAME = `velodrive-cache-${CACHE_VERSION}`;
 
 // Files to make available offline
